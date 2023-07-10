4a39b9f6ae113c2a43c29771ea43de8208c427ae
Henrique <henriquesilva254@gmail.com>
2023-07-10 16:01:41 -0300

    myWalelt: deploy on Render

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ app.use(router)
 
 
 // Run Server
-const PORT = 5000
-app.listen(PORT, () => console.log('Server Run'))
-
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+	console.log(`Servidor rodando na porta ${port}`)
+})
