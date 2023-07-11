3b7746f45c256df22c8d2c322ba4e6e5901e5397
Henrique <henriquesilva254@gmail.com>
2023-07-11 16:46:59 -0300

    myWalelt: correcao de bugs

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -27,6 +27,7 @@ export async function signIn(req, res){
 
   try {
       const user = await db.collection('users').findOne({email})
+      if(!user) return res.status(404).send("usuario nao existe")
       if(user && bcrypt.compareSync(password, user.password)){
           const token = uuid()
           await db.collection('session').insertOne({token, name: user.name, userId:user._id})
